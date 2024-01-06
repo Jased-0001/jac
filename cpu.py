e0128ba2e9dc670fb765a6957f7a0ccbaf035e74
Jase Williams <jased@jased.xyz>
2024-01-06 10:57:17 -0500

    Created comments

--- cpu.py
+++ cpu.py
@@ -4,12 +4,17 @@ rtt  = 0x00
 hld  = 0x00
 
 def run(instruction):
+    # get access to registers
     global ro
     global rt
     global rtt
     global hld
 
+    # was hold changed?
     if hld != 0x00:
+        # yes it was. Store the data in RTT and put the hld register in the instruction
+        # so the held instruction actually gets run. Set hld to 0xff to signal that 
+        # things were actually done
         rtt = instruction
         instruction = hld
         hld = 0xff
@@ -17,17 +22,14 @@ def run(instruction):
     match instruction:
         case 0x00:
             # NOP
-            print("NOP", end="")
+            # No operation. Pass and do nothing
             pass
-
-        case 0x01:
-            # TST
-            print("TST", end="")
-
 
 
         case 0x10:
             # STRO
+            # Store whatever is in RTT in RO.
+            # Takes two cycles, one to set up and one to store
             if hld == 0xff:
                 ro = rtt
                 rtt = 0x00
@@ -37,6 +39,8 @@ def run(instruction):
 
         case 0x11:
             # STRT
+            # Store whatever is in RTT in RT
+            # Same as 0x10
             if hld == 0xff:
                 rt = rtt
                 rtt = 0x00
@@ -46,6 +50,7 @@ def run(instruction):
             
         case 0x12:
             # STRTT (please never use)
+            # This is easier because we dont need to do anything to RTT
             if hld == 0xff:
                 hld = 0x00
             else:
@@ -55,17 +60,18 @@ def run(instruction):
                 
         case 0x20:
             # ADD
-
+            # Adds ro and rt and store it in ro
             ro = ro + rt
 
         case 0x21:
             # SUB
-
+            # Subtracts ro and rt and stores it in ro
             ro = ro - rt
 
         
         case 0x30:
             #PRN
+            # Print RO's character
             print(chr(ro), end="")
 
         case _:
